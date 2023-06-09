5bc80f0649f7d7e19c099a30dae25e25a8717e96
OstoaLeonardo <imlost@micorreo.upp.edu.mx>
2023-06-08 23:42:28 -0600

    Fragments Java

--- app/src/main/java/com/example/inventarioapp/MainActivity.java
+++ app/src/main/java/com/example/inventarioapp/MainActivity.java
@@ -7,14 +7,25 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String PREFS_NAME = "MyPrefsFile";
     private static final String KEY_FIRST_RUN = "firstRun";
+
+    InventoryFragment inventario = new InventoryFragment();
+    AddInventoryFragment agregarInventario = new AddInventoryFragment();
+    RemoveInventoryFragment quitarInventario = new RemoveInventoryFragment();
+    AddProductFragment nuevoProducto = new AddProductFragment();
+    AddUserFragment nuevoUsuario = new AddUserFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +53,42 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        BottomNavigationView navigation = findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(inventario);
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.inventario:
+                    loadFragment(inventario);
+                    return true;
+                case R.id.agregar:
+                    loadFragment(agregarInventario);
+                    return true;
+                case R.id.quitar:
+                    loadFragment(quitarInventario);
+                    return true;
+                case R.id.nuevoProducto:
+                    loadFragment(nuevoProducto);
+                    return true;
+                case R.id.nuevoUsuario:
+                    loadFragment(nuevoUsuario);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.commit();
+    }
+
     public void logout() {
 
         Intent intent = new Intent(this, Login.class);
