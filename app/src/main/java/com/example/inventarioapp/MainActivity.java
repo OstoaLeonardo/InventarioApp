77c8e45c30f25fe0ac938a92c185f06adc60833d
OstoaLeonardo <imlost@micorreo.upp.edu.mx>
2023-06-08 23:14:36 -0600

    BottomNav

--- app/src/main/java/com/example/inventarioapp/MainActivity.java
+++ app/src/main/java/com/example/inventarioapp/MainActivity.java
@@ -4,14 +4,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    InventoryFragment inventario = new InventoryFragment();
+    AddInventoryFragment agregarInventario = new AddInventoryFragment();
+    RemoveInventoryFragment quitarInventario = new RemoveInventoryFragment();
+    AddProductFragment nuevoProducto = new AddProductFragment();
+    AddUserFragment nuevoUsuario = new AddUserFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +50,45 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
-    }
-    public void logout() {
 
+        BottomNavigationView navigation = findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(inventario);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.inventario:
+                    loadFragment(inventario);
+                    return true;
+                case R.id.agregar:
+                    loadFragment(agregarInventario);
+                    return true;
+                case R.id.quitar:
+                    loadFragment(quitarInventario);
+                    return true;
+                case R.id.nuevoProducto:
+                    loadFragment(nuevoProducto);
+                    return true;
+                case R.id.nuevoUsuario:
+                    loadFragment(nuevoUsuario);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.commit();
+    }
+
+    public void logout() {
         Intent intent = new Intent(this, Login.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
-       // Cierra la actividad actual para evitar que el usuario vuelva atrás
     }
-
 }
